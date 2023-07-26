6dfdd1f642063d6b7b361799c2e0778e6d89489c
mariem-negra/janyour <mariem.negra@esprit.tn>
2023-07-27 00:13:38 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+/*alert("this is an alert");
+prompt("please enter your name");*/
+/*x=prompt("please enter your name");
+alert("x"); mghir mtekteb el alert*/
+//y=confirm("do you want to close this?")
+/*alert(y)//y mghir deux cotes fi alert ta3tina el valeurmt3 y yema true or false*/
+//console.log(y)//temchy tchouf valeur y fi console
+/*x=prompt("Donner votre adresse mail")
+y="mariem.negra@esprit.tn"
+h=prompt("donner votre mdp")
+yy="221JFT3128"
+while(x!=y && h!=yy)
+{alert("donner votre mail")
+}*/
+function addnumbers()
+{
+    var num1=parseFloat(document.getElementById("num1").value);
+    var num2=parseFloat(document.getElementById("num2").value);
+    var som=num1+num2
+    document.getElementById("result").innerHTML="result:"+som}
